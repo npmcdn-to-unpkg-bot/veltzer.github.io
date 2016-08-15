ab0b203a4cfece8421ad62fcd5e1317b04a483d0
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-15 17:03:47 +0300

    better support for ubuntu packages

--- scripts/tools.py
+++ scripts/tools.py
@@ -10,32 +10,48 @@ ubuntu to compile and work with this package.
 ###########
 import subprocess # for check_call, DEVNULL
 import os.path # for isfile
+import json # for load
 
 ##############
 # parameters #
 ##############
 # debug?
-debug=False
+do_debug=False
+do_msg=True
+
+#############
+# functions #
+#############
+def debug(s):
+    if do_debug:
+        print(s)
+
+def msg(s):
+    if do_msg:
+        print(s)
 
 ########
 # code #
 ########
-if os.path.isfile('packages'):
-	if debug:
-		print('installing npm packages...')
-	packs=open('packages').read().splitlines()
-	args=['sudo','apt-get','install','--assume-yes']
-	args.extend(packs)
-	subprocess.check_call(
-		args,
-		stdout=subprocess.DEVNULL,
-		stderr=subprocess.DEVNULL
-	)
+if os.path.isfile('ubuntu.json'):
+    msg('installing packages...')
+    with open('ubuntu.json') as f:
+        d=json.load(f)
+    packs=[p['name'] for p in d['packages']]
+    debug(packs)
+    args=['sudo','apt-get','install','--assume-yes']
+    args.extend(packs)
+    subprocess.check_call(
+        args,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
 
 if os.path.isfile('package.json'):
-	if debug:
-		print('installing npm packages...')
-	subprocess.check_call([
-		'npm',
-		'install',
-	], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    msg('installing npm packages...')
+    '''
+    subprocess.check_call([
+        'npm',
+        'install',
+    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    '''
